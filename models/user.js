3bf48f239c2668ea480c2f69acb02281a8a67f9c
Lora <lorat1403@gmail.com>
2022-12-24 16:05:12 +0200

    mainhw

--- models/user.js
+++ models/user.js
@@ -8,10 +8,10 @@ const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const userSchema = new Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
+    // name: {
+    //   type: String,
+    //   required: true,
+    // },
     email: {
       type: String,
       match: emailRegexp,
@@ -23,6 +23,15 @@ const userSchema = new Schema(
       minlength: 6,
       required: [true, "Password is required"],
     },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -30,7 +39,7 @@ const userSchema = new Schema(
 userSchema.post("save", handleMongooseError);
 
 const registerSchema = Joi.object({
-  name: Joi.string().required(),
+  // name: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });

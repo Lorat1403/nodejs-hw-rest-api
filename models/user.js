b1daf16b61874161040180caddf2311dc5794806
Lora <lorat1403@gmail.com>
2022-12-26 10:39:13 +0200

    fix rout

--- models/user.js
+++ models/user.js
@@ -8,10 +8,6 @@ const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const userSchema = new Schema(
   {
-    // name: {
-    //   type: String,
-    //   required: true,
-    // },
     email: {
       type: String,
       match: emailRegexp,
@@ -39,7 +35,6 @@ const userSchema = new Schema(
 userSchema.post("save", handleMongooseError);
 
 const registerSchema = Joi.object({
-  // name: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
